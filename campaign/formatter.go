bdd916cf77aa9ee442aa7813ea94f46995054255
irfan-arrosid <irfanarrosid4@gmail.com>
2023-06-30 23:16:58 +0700

    Feat: Create campaign endpoint

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,40 @@
 package campaign
+
+type CampaignFormatter struct {
+	Id               int    `json:"id"`
+	UserId           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{}
+
+	campaignFormatter.Id = campaign.Id
+	campaignFormatter.UserId = campaign.UserId
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.ShortDescription
+	campaignFormatter.GoalAmount = campaign.GoalMount
+	campaignFormatter.CurrentAmount = campaign.CurrentMount
+	campaignFormatter.ImageURL = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageURL = campaign.CampaignImages[0].Filename
+	}
+
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign((campaign))
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
