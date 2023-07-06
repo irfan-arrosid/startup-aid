8b6bd0f3a8f38188cf7960fdc48c6dbeb0e2e404
irfan-arrosid <irfanarrosid4@gmail.com>
2023-07-06 14:37:26 +0700

    Feat: Create get transaction by userId repository

--- transaction/entity.go
+++ transaction/entity.go
@@ -3,6 +3,7 @@ package transaction
 import (
 	"time"
 
+	"github.com/irfan-arrosid/startup-aid/campaign"
 	"github.com/irfan-arrosid/startup-aid/user"
 )
 
@@ -14,6 +15,7 @@ type Transaction struct {
 	Status     string
 	Code       string
 	User       user.User
+	Campaign   campaign.Campaign
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
--- transaction/repository.go
+++ transaction/repository.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Repository interface {
 	FindByCampaignId(campaignId int) ([]Transaction, error)
+	FindByUserId(userId int) ([]Transaction, error)
 }
 
 type repository struct {
@@ -18,6 +19,17 @@ func (r *repository) FindByCampaignId(campaignId int) ([]Transaction, error) {
 	var transactions []Transaction
 
 	err := r.db.Preload("User").Where("campaign_id = ?", campaignId).Order("id desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (r *repository) FindByUserId(userId int) ([]Transaction, error) {
+	var transactions []Transaction
+
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userId).Find(&transactions).Error
 	if err != nil {
 		return transactions, err
 	}
