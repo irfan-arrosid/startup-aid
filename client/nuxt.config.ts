03bcef96befba6e0bac80456760bf6993183e4fa
irfan-arrosid <irfanarrosid4@gmail.com>
2023-07-08 13:57:21 +0700

    Feat: Create landing page

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,7 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  pages: true,
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'StartUp Aid',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: ''},
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
+        },
+      ],
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
