f0488b3aea9f5bab54a33607b917884ed57a047f
irfan-arrosid <irfanarrosid4@gmail.com>
2023-07-08 19:03:13 +0700

    Feat: Create login and register page

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .auth-background {
     background-image: url('/sign-in-background.jpg');
     background-position: center;
